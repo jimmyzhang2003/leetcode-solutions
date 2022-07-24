0d82f3d0433d8576076c6c85f3c3d70b00335b26
Jimmy Zhang <jimmyzhang2003@gmail.com>
2022-07-24 13:11:22 -0400

    finished valid parentheses and updated Week 1 README

---
+++ week-1/2_valid_parentheses.py
@@ -0,0 +1,16 @@
+# Link: https://leetcode.com/problems/valid-parentheses/
+# Time Complexity: O(N)
+# Space Complexity: O(N)
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        mapping = {')':'(', '}':'{', ']':'['}
+        stack = []
+        
+        for bracket in s:
+            if bracket in '({[':
+                stack.append(bracket)
+            elif len(stack) == 0 or stack.pop() != mapping[bracket]:
+                return False
+        
+        return len(stack) == 0
---
+++ week-1/README.md
@@ -0,0 +1,4 @@
+# Week 1
+
+1. [Two Sum [EASY]](https://leetcode.com/problems/two-sum/)
+2. [Valid Parentheses [EASY]](https://leetcode.com/problems/valid-parentheses/)
