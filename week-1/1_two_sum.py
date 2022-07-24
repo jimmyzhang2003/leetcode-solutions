8c52c7b6f1b0a835cfd058a6355ab1f0afaec1db
Jimmy Zhang <jimmyzhang2003@gmail.com>
2022-07-24 12:47:25 -0400

    finished two sum

---
+++ week-1/1_two_sum.py
@@ -0,0 +1,14 @@
+# Link: https://leetcode.com/problems/two-sum/
+# Time Complexity: O(N)
+# Space Complexity: O(N)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        
+        for i, x in enumerate(nums):
+            if target - x in seen:
+                return [i, seen[target-x]]
+            seen[x] = i
+        
+        return []
