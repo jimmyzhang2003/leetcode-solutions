e78e3db29cbe872f61b12fd791578a621d98e6d3
Jimmy Zhang <jimmyzhang2003@gmail.com>
2022-07-25 11:05:39 -0400

    Finished Merge Two Sorted Linked Lists

---
+++ week-1/3_merge_two_sorted_linked_lists.py
@@ -0,0 +1,33 @@
+# Link: https://leetcode.com/problems/merge-two-sorted-lists/
+# Time Complexity: O(N+M)
+# Space Complexity: O(N+M)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode: 
+        head = ListNode()
+        p = head
+        
+        while l1 or l2:
+            if l1 and l2:
+                if l1.val > l2.val:
+                    p.next = l2
+                    l2 = l2.next
+                else:
+                    p.next = l1
+                    l1 = l1.next
+                p = p.next
+            elif l1:
+                p.next = l1
+                l1 = l1.next
+                p = p.next
+            else:
+                p.next = l2
+                l2 = l2.next
+                p = p.next
+    
+        return head.next
--- week-1/README.md
+++ week-1/README.md
@@ -2,3 +2,4 @@
 
 1. [Two Sum [EASY]](https://leetcode.com/problems/two-sum/)
 2. [Valid Parentheses [EASY]](https://leetcode.com/problems/valid-parentheses/)
+3. [Merge Two Sorted Linked Lists [EASY]](https://leetcode.com/problems/merge-two-sorted-lists/)
