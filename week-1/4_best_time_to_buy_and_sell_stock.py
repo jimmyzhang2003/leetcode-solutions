56e7b8e4f99902d96f351551a21788c21b98f5c1
Jimmy Zhang <jimmyzhang2003@gmail.com>
2022-08-01 00:00:30 -0400

    Added Kadane's Algorithm solution to Best Time to Buy and Sell Stock

--- week-1/4_best_time_to_buy_and_sell_stock.py
+++ week-1/4_best_time_to_buy_and_sell_stock.py
@@ -16,3 +16,15 @@ class Solution:
             s += 1    
     
         return currMax
+
+#Kadane's Algorithm
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        currMin = prices[0]
+        
+        for i in range(1, len(prices)):
+            currMin = min(currMin, prices[i])
+            profit = max(profit, prices[i]-currMin)
+            
+        return profit
