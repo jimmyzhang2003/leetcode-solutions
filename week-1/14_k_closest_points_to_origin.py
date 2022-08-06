053b5234b9f194f485713fff12fe7026d3c79360
Jimmy Zhang <jimmyzhang2003@gmail.com>
2022-08-06 01:55:28 -0400

    Added quick select solution to k closest points

--- week-1/14_k_closest_points_to_origin.py
+++ week-1/14_k_closest_points_to_origin.py
@@ -19,7 +19,7 @@ class Solution:
             
         return [x[1] for x in heap]
 
-## Min Heap: O(N + k log N) time complexity, O()
+## Min Heap: O(N + k log N) time complexity, O(N) space complexity
 import heapq
 
 class Solution:
@@ -35,4 +35,42 @@ class Solution:
            
         return [heapq.heappop(heap)[1] for _ in range(k)]
 
-## Quick Select
+## Quick Select: O(N) average case and O(N^2) worse case time complexity, O(1) space complexity
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        def dist(point):
+            return point[0] ** 2 + point[1] ** 2
+        
+        def partition(points, l, r):
+            #pick a random element to be the pivot
+            pivot = random.randint(l, r)
+            
+            #swap pivot to the end
+            points[r], points[pivot] = points[pivot], points[r]
+            
+            ptr = l
+            
+            for i in range(l, r):
+                if dist(points[i]) <= dist(points[r]):
+                    points[ptr], points[i] = points[i], points[ptr]
+                    ptr += 1
+            
+            #swap pivot back
+            points[ptr], points[r] = points[r], points[ptr]
+            
+            return ptr
+    
+        l, r = 0, len(points) - 1
+
+        while l < r:
+            mid = partition(points, l, r)
+            
+            if mid < k:
+                l = mid + 1
+            elif mid > k:
+                r = mid - 1
+            else:
+                break
+            
+        return points[:k]
+
