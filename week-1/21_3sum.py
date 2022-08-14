9c8731a726c29beea3d90eed065d6284ef04b340
Jimmy Zhang <jimmyzhang2003@gmail.com>
2022-08-13 22:06:21 -0400

    Added another brute force solution for 3Sum

--- week-1/21_3sum.py
+++ week-1/21_3sum.py
@@ -20,6 +20,29 @@ class Solution:
         
         return res
 
+# Time Complexity: O(N^3)
+# Space Complexity: O(N^2)
+# (Brute Force without Set)
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        
+        nums.sort()
+        
+        for i in range(0, len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, len(nums) - 1):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                for k in range(j + 1, len(nums)):
+                    if k > j + 1 and nums[k] == nums[k - 1]:
+                        continue
+                    numsSet = set([nums[i], nums[j], nums[k]])
+                    if i != j and i != k and j != k and numsSet and nums[i] + nums[j] + nums[k] == 0:
+                        res.append([nums[i], nums[j], nums[k]])
+        
+        return res
 
 # Time Complexity: O(N^2)
 # Space Complexity: O(N^2)
