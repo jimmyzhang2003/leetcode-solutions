a0bd8d9496cea1954577a27774668aa2222da958
Jimmy Zhang <jimmyzhang2003@gmail.com>
2022-12-28 13:09:02 -0500

    Finished Generate Parentheses

---
+++ backtracking/generate_parentheses.py
@@ -0,0 +1,21 @@
+# Link: https://leetcode.com/problems/generate-parentheses/
+
+# Time Complexity: O(4^n / √n)
+# Space Complexity: O(4^n / √n)
+# (Backtracking)
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        def backtrack(nLeft, nRight, res, curr):
+            if not nLeft:
+                res.append("".join(curr) + nRight * ")")
+                return
+
+            backtrack(nLeft-1, nRight, res, curr + ["("])
+
+            if nRight > nLeft:
+                backtrack(nLeft, nRight-1, res, curr + [")"])
+
+        res = []
+        backtrack(n, n, res, [])
+
+        return res
